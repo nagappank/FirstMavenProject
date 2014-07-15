be2e9f227e8cba1b11813b6ddb2a71da6615a18d
nagappank <krishnan.nagappan@gmail.com>
2014-07-15 10:14:07 +0200

    First Commit in Maven Project

---
+++ MyJUnits_Maven_Project/src/main/java/com/cgi/selenium/MyJUnits_Maven_Project/App.java
@@ -0,0 +1,13 @@
+package com.cgi.selenium.MyJUnits_Maven_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
